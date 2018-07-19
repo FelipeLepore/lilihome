af5e3862b2f1430b88ccb7f1e63e4a3491839382
Pedro de Albuquerque <pedrowevernek@gmail.com>
2018-07-18 22:22:00 -0300

    Mais ajustes e finalização da home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,6 +8,21 @@ import { BreadcrumbService } from '../../shared/breadcrumb.service';
 })
 export class HomeComponent implements OnInit {
 
+  tableMock = [
+    { purchase: 123456789, date: '27/09/2018', status: 'EM TRÂNSITO', price: 'R$10.665,75' },
+    { purchase: 123456789, date: '27/09/2018', status: 'EM TRÂNSITO', price: 'R$10.665,75' },
+    { purchase: 123456789, date: '27/09/2018', status: 'EM TRÂNSITO', price: 'R$10.665,75' },
+    { purchase: 123456789, date: '27/09/2018', status: 'EM TRÂNSITO', price: 'R$10.665,75' },
+    { purchase: 123456789, date: '27/09/2018', status: 'EM TRÂNSITO', price: 'R$10.665,75' }
+  ]
+
+  productsListMock = [
+    { image: 'http://marcheartdevie.com.br/shop/6102-large_default/CADEIRA-MACCHIATO.jpg', name: 'Vienna Convertible Chaise Lounge', price: 'R$13.419,99' },
+    { image: 'http://marcheartdevie.com.br/shop/6102-large_default/CADEIRA-MACCHIATO.jpg', name: 'Vienna Convertible Chaise Lounge', price: 'R$13.419,99' },
+    { image: 'http://marcheartdevie.com.br/shop/6102-large_default/CADEIRA-MACCHIATO.jpg', name: 'Vienna Convertible Chaise Lounge', price: 'R$13.419,99' },
+    { image: 'http://marcheartdevie.com.br/shop/6102-large_default/CADEIRA-MACCHIATO.jpg', name: 'Vienna Convertible Chaise Lounge', price: 'R$13.419,99' }
+  ]
+
   constructor(private breadCrumb: BreadcrumbService) { }
 
   ngOnInit() {
